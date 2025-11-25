035f1eb7c744d004789667db7d209b4c11aac0a5
gabrielSalvador505 <reissalvador2004@gmail.com>
2025-11-25 09:39:21 -0300

    Commit 10

--- Movie.java
+++ Movie.java
@@ -43,4 +43,12 @@ public class Movie {
       }
       return thisAmount;
   }
+    public int getFrequentRenterPoints(int daysRented) {
+        // add frequent renter points
+        int frequentRenterPoints = 1;
+        // add bonus for a two day new release rental
+        if ((getPriceCode() == NEW_RELEASE) &&
+                daysRented > 1) frequentRenterPoints ++;
+        return frequentRenterPoints;
+    }
 }
